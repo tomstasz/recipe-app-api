8f9eebce05accbdcae2676faa3f2462a40fa7022
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-06 15:20:08 +0100

    create test_models module in tests folder, add create_user_with_email test

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@onet.pl'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))  # hasło szyfrowane, dlatego używamy funkcji check_password()
+
