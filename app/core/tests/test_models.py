2de75c3f2330ed2ff3c284230e7d0cc027007cea
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-06 22:33:12 +0100

    some flake8 fixes

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,8 +8,8 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = 'test@onet.pl'
         password = 'Testpass123'
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(email=email,
+                                                    password=password)
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password))  # hasło szyfrowane, dlatego używamy funkcji check_password()
-
+        self.assertTrue(user.check_password(password))
