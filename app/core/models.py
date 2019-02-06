5d3a84e8da2592e75200d72b2d42c5863fa09c3a
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-06 17:12:47 +0100

    add custom User model

--- app/core/models.py
+++ app/core/models.py
@@ -13,7 +13,16 @@ class UserManager(BaseUserManager):
         return user
 
 
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
 
 
 
