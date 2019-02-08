141383f7965ed9fd5853c7e4d5f4db7cd58b0da3
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-08 15:56:52 +0100

    update admin to support custom user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -6,13 +6,14 @@ from django.urls import reverse
 class AdminSiteTests(TestCase):
 
     def setUp(self):
+        u_model = get_user_model()
         self.client = Client()
-        self.admin_user = get_user_model().objects.create_superuser(email='admin@onet.pl',
-                                                                    password='test123')
+        self.admin_user = u_model.objects.create_superuser('admin@onet.pl',
+                                                           'test123')
         self.client.force_login(self.admin_user)
-        self.user = get_user_model().objects.create_user(email='test@interia.pl',
-                                                         password='test123',
-                                                         name='Test user full name')
+        self.user = u_model.objects.create_user('test@interia.pl',
+                                                'test123',
+                                                name='Test user full name')
 
     def test_users_listed(self):
         """Test that users are listed on user page"""
@@ -21,3 +22,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,4 +34,3 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
