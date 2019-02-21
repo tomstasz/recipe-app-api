df5e605e9b237746f2f8d7c1c38f7a7d2cdb2a11
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-21 16:05:43 +0100

    add test to create a token if credentials are invalid

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -74,3 +74,17 @@ class PublicUserApiTests(TestCase):
 
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test that token is not created if invalid credentials are given"""
+        create_user(email='test@onet.pl', password='testpass')
+
+        payload = {
+            'email': 'test@onet.pl',
+            'password': 'wrong_pass',
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
