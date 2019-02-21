0f61607be3418d7f158a77adaa0ff32900f2f6d5
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-21 13:30:06 +0100

    add test creating user that already exists to PublicUser TestCase

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -33,3 +33,16 @@ class PublicUserApiTests(TestCase):
         user = get_user_model().objects.get(**res.data)
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """Test creating user that already exists fails"""
+        payload = {
+            'email': 'test@onet.pl',
+            'password': 'testpass',
+        }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
