c41f549159239ae7377f46de941d1b5e42e83015
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-21 15:30:07 +0100

    add CreateUserView and urls

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -56,6 +56,6 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']).exists()
         self.assertFalse(user_exists)
-
