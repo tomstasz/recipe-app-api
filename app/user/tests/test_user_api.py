5436e42573505672a4d2512825192fd035051ff0
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-22 11:49:36 +0100

    add test to check if post is not allowed on user profile

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -137,3 +137,9 @@ class PrivateUserApiTests(TestCase):
             'name': self.user.name,
             'email': self.user.email
         })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
