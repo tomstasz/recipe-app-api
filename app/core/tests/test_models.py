8320827e516617153535e0dc6db9ca2518660105
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-22 17:07:51 +0100

    add and register ingredient model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .. import models
+from core import models
 
 
 def sample_user(email='test@onet.pl', password='testpass'):
@@ -49,3 +49,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
