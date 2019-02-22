2b2ebb373396767aacf1dc01165f1f1d2e57a8c4
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-22 17:44:14 +0100

    add test to check if ingredients are available for the authenticated user only

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -54,3 +54,17 @@ class PrivateIngredientsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        """Test that only authenticated user gets list of ingredients"""
+        user2 = get_user_model().objects.create_user('test2@onet.pl', 'testpass2')
+
+        Ingredient.objects.create(user=user2, name='Vinegar')
+
+        ingredient = Ingredient.objects.create(user=self.user, name='Garlic')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
