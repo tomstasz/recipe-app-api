8c9bfe5b0a3c5c630374af3366f50189aaf0d969
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-22 17:56:38 +0100

    register ingredient view set in urls

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 router = DefaultRouter()  # automaticly generates urls for viewset
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
