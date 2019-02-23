49e8a3723878290b459b736491d5ee738900e23f
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-02-23 16:26:53 +0100

    add test to create recipe with ingredients

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -138,3 +138,26 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(tags.count(), 2)
         self.assertIn(tag1, tags)
         self.assertIn(tag2, tags)
+
+    def test_create_recipe_with_ingredients(self):
+        """Test creating a recipe with ingredients"""
+        ingredient1 = sample_ingredient(user=self.user, name='Apple')
+        ingredient2 = sample_ingredient(user=self.user, name='Orange')
+
+        payload = {
+            'title': 'Fruit salad',
+            'ingredients': [ingredient1.id, ingredient2.id],
+            'time_minutes': 10,
+            'price': 7.00
+        }
+        res = self.client.post(RECIPES_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        recipe = Recipe.objects.get(id=res.data['id'])
+        ingredients = recipe.ingredients.all()
+
+        self.assertEqual(ingredients.count(), 2)
+        self.assertIn(ingredient1, ingredients)
+        self.assertIn(ingredient2, ingredients)
+
